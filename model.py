e326b8481360636f478369cadf2b010d88d7964a
danielkliu07-beep <danielkliu07@gmail.com>
2026-07-14 21:51:00 -0400

    Added classes

---
+++ model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class TDNNLayer(nn.Module):
+
+    def __init__(self, input_dim, output_dim, context):
+        super().__init__()
+    
+    def forward(self, x):
+
+        return x
+
+
+
+
+class XVectorModel(nn.Module):
+
+    def __init__(self):
+        pass
+
+
+
+
+class AAMSoftmaxLoss(nn.Module):
+
+    def __init__(self, margin, scale):
+        pass
+
+
